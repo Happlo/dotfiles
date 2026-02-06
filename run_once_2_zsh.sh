1e5294102e85e8babcbe50bff3de1e56ea3d60e8
Eric Björklund <bjoerklunderic@gmail.com>
2026-02-06 16:18:18 +0100

    Make sure zshrc is not overwritten

--- run_once_2_zsh.sh
+++ run_once_2_zsh.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 set -euo pipefail
+cp ~/.zshrc ~/.zshrc.backup
 
 curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh | sh
 
 # Install as default
 chsh -s "$(which zsh)"
+
+# Restore previous .zshrc
+mv ~/.zshrc.backup ~/.zshrc
