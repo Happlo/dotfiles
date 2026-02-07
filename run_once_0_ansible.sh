9f62f7aa3e68c11d3fd62e070b6d2e18477b50b2
Eric Björklund <bjoerklunderic@gmail.com>
2026-02-07 08:18:11 +0100

    Moving installation to ansible

---
+++ run_once_0_ansible.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+echo "==> Updating apt index"
+sudo apt-get update
+
+echo "==> Installing Ansible"
+sudo apt-get install -y ansible
+
+echo "==> Ansible installed"
+
+echo "==> Running Ansible playbook setup"
+ansible-playbook -v ~/.ansible/setup.yml
+
+echo "==> Setup complete"
