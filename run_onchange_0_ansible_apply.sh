cdf7df7b5aafd000626d201329c585d848cd09be
Eric Björklund <bjoerklunderic@gmail.com>
2026-02-08 13:41:46 +0100

    Separate change so that ansible is run on every change

--- run_onchange_0_ansible_apply.sh
+++ run_onchange_0_ansible_apply.sh
@@ -1,14 +1,6 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-echo "==> Updating apt index"
-sudo apt-get update
-
-echo "==> Installing Ansible"
-sudo apt-get install -y ansible
-
-echo "==> Ansible installed"
-
 echo "==> Running Ansible playbook setup"
 ansible-playbook ~/.ansible/setup.yml
 
